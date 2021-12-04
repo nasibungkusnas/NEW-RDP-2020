a3dfaed99e53d771ba1c66fdbd071264c6520800
nasibungkusnas <88348260+nasibungkusnas@users.noreply.github.com>
2021-12-04 08:50:20 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-net user administrator wwshekher /add >nul
+net user administrator 123456@wW /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -12,6 +12,6 @@ echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @Oshekher
+echo Password: 123456@wW
 echo Silahkan Login Ke RDP Anda!!
 ping -n 10 127.0.0.1 >nul
